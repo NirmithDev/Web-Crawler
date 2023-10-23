2c72c2e25214a7dbc4e452cb0561859463466777
Ali Hassan Sharif <71347625+AliSharif12354@users.noreply.github.com>
2023-10-23 18:32:13 -0400

    PageRank for Fruit pages

--- crawly.js
+++ crawly.js
@@ -7,7 +7,7 @@ const {Matrix} = require("ml-matrix");
 const cheerio = require('cheerio');
 
 //Connection URL
-const mongourl = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.6";
+const mongourl = "mongodb+srv://ali12354:ali12354@cluster0.cpdw4uq.mongodb.net/?retryWrites=true&w=majority";
 
 //Create a MongoClient instance
 const client = new MongoClient(mongourl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -174,70 +174,129 @@ crawler.on('drain', async function(){
     try {
         console.log("Done!");
         console.log(`We have crawled through ${pageCounter} pages...`);
+
+
+
+        /*Page Rank*/
     
-        //Initialize 
-        const a = 0.1;
-
         //Transition probability matrix
-        let P = Matrix.zeros(pageCounter, pageCounter);
-        for (const pageData of tempData) {
-            pageData.pagerank = 1 / pageCounter;
-            //pageData.adjacencyMatrix = new Array(N).fill(0);
+        P = Matrix.zeros(tempData.length, tempData.length); // Could just do 500, 500
+
+        a = 0.1
+
+        //PageRank vector and Convergence threshold
+        x0 = Matrix.ones(1, tempData.length);
+        let t = 0.0001;
+        
+        // Index data for matrix building purposes
+        for(i = 0; i < tempData.length; i++){
+            tempData[i].index = i;
         }
-        for (let i = 0; i < pageCounter; i++) {
-            let numOnes = 0;
-            for (let j = 0; j < pageCounter; j++) {
-                let currentPage = tempData[i];
-                let comparedPage = tempData[j];
-                if (currentPage.connectedPages.includes(comparedPage.url)) {
-                    P.set(i,j,1);
-                    numOnes += 1;
-                } else {
-                    P.set(i,j,0);
-                }
-            }
-            if (numOnes <= 0) {
-                for (let j = 0; j < pageCounter; j++) {
-                    P.set(i,j,(1/pageCounter));
-                }
-            } else {
-                for (let j = 0; j < pageCounter; j++) {
-                    //check if i!=j then proceed
-                    P.set(i,j,(P.get(i,j)/numOnes));
-                }
+
+        // Build transition probability matrix
+        for (i = 0; i < P.rows; i++) {
+            pageNum = tempData[i].index; // Get index
+            linkedTo = tempData[i].connectedPages; 
+            for (j = 0; j < P.columns; j++) {
+              if(linkedTo.includes(tempData[j].url)){ // If the page is linked to a page at j, add info to matrix
+                pageNumOfLinkedTo = tempData[j].index;
+                //The probability calculation
+                P.set(pageNum, pageNumOfLinkedTo, P.get(pageNum, pageNumOfLinkedTo) + 1/linkedTo.length); 
+              }
             }
         }
-        //I changed the 1-a for the dampin factor
-        P = Matrix.mul(P, (1-a));
-        P = Matrix.add(P, Matrix.mul(Matrix.ones(pageCounter, pageCounter),(a/pageCounter))); //see if you can change
 
-        //Initial PageRank vector
-        x0 = Matrix.zeros(1,pageCounter);
-        x0.set(0,0,1);
-        let curVector = x0;
-        let lastVector = null;
+        console.log("Transition Probability Matrix: \n", P);
 
-        let eud = 1;
-        let iteration = 0;
-        while (eud > 0.0001 && iteration < 1000) {
-
-            lastVector = curVector;
-            curVector = curVector.mmul(P);
-
-            eud = 0;
-            for (let i = 0; i < pageCounter; i++) {
-                for (let j = 0; j < pageCounter; j++) {
-                    eud += (curVector.get(0,i)-lastVector.get(0,j)) * (curVector.get(0,i)-lastVector.get(0,j));
-                }
-            }
-            eud = Math.sqrt(eud);
-            iteration += 1;
-            // console.log(`Iteration: ${iteration},       eud: ${eud}`);
+        difference = 10000; //Dummy value to start
+        while(difference > t){
+            x1 = x0.mmul(P);
+            difference = x0.sub(x1).abs().sum();
+            x0 = x1;
         }
 
-        for (let i = 0; i < pageCounter; i++) {
-            tempData[i].pageRank = curVector.get(0,i);
+        //Finish PageRank Calculation
+        x0.div(x0.sum()); // Dont know if this is needed, had this in my Lab 5 code
+        x0.mul(1 - a); // Create a 10% chance of teleportation, a is defined abovr as 0.1
+
+        // Add PageRank to each page!
+        for(i = 0; i < x0.size; i++) {
+            // NOT SURE if indexing is correct here! Might need a nested loop and check tempDataPersonal[j].index == i
+            tempData[i].pageRank = x0.get(0, i); 
         }
+
+        console.log("PageRank Sum: ", x0.sum());
+        console.log("PageRank Array: ", x0);
+
+        /* Nirmith's page rank calculation below */
+
+        //Initialize 
+        // const a = 0.1;
+
+        // //Transition probability matrix
+        // let P = Matrix.zeros(pageCounter, pageCounter);
+        // for (const pageData of tempData) {
+        //     pageData.pagerank = 1 / pageCounter;
+        //     //pageData.adjacencyMatrix = new Array(N).fill(0);
+        // }
+        // for (let i = 0; i < pageCounter; i++) {
+        //     let numOnes = 0;
+        //     for (let j = 0; j < pageCounter; j++) {
+        //         let currentPage = tempData[i];
+        //         let comparedPage = tempData[j];
+        //         if (currentPage.connectedPages.includes(comparedPage.url)) {
+        //             P.set(i,j,1);
+        //             numOnes += 1;
+        //         } else {
+        //             P.set(i,j,0);
+        //         }
+        //     }
+        //     if (numOnes <= 0) {
+        //         for (let j = 0; j < pageCounter; j++) {
+        //             P.set(i,j,(1/pageCounter));
+        //         }
+        //     } else {
+        //         for (let j = 0; j < pageCounter; j++) {
+        //             //check if i!=j then proceed
+        //             P.set(i,j,(P.get(i,j)/numOnes));
+        //         }
+        //     }
+        // }
+        // //I changed the 1-a for the dampin factor
+        // P = Matrix.mul(P, (1-a));
+        // P = Matrix.add(P, Matrix.mul(Matrix.ones(pageCounter, pageCounter),(a/pageCounter))); //see if you can change
+
+        // //Initial PageRank vector
+        // x0 = Matrix.zeros(1,pageCounter);
+        // x0.set(0,0,1);
+        // let curVector = x0;
+        // let lastVector = null;
+
+        // let eud = 1;
+        // let iteration = 0;
+        // while (eud > 0.0001 && iteration < 1000) {
+
+        //     lastVector = curVector;
+        //     curVector = curVector.mmul(P);
+
+        //     eud = 0;
+        //     for (let i = 0; i < pageCounter; i++) {
+        //         for (let j = 0; j < pageCounter; j++) {
+        //             eud += (curVector.get(0,i)-lastVector.get(0,j)) * (curVector.get(0,i)-lastVector.get(0,j));
+        //         }
+        //     }
+        //     eud = Math.sqrt(eud);
+        //     iteration += 1;
+        //     // console.log(`Iteration: ${iteration},       eud: ${eud}`);
+        // }
+
+        // for (let i = 0; i < pageCounter; i++) {
+        //     tempData[i].pageRank = curVector.get(0,i);
+        // }
+
+
+
+
 /*
         let top25 = tempData.slice();
         top25.sort((a, b) => b.pageRank - a.pageRank);
